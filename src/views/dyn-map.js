207136357b744ba6d71380f6493248470b5fd885
zigzatuzoo <zigzatuzoo@zigzatuzoo.xyz>
2022-10-12 14:07:51 -0700

    FixDynmap

--- src/views/dyn-map.js
+++ src/views/dyn-map.js
@@ -9,17 +9,9 @@ const DynMap = (props) => {
   return (
     <div className="dyn-map-container">
       <Helmet>
-        <title>DynMap - RTFM Server</title>
-        <meta property="og:title" content="DynMap - RTFM Server" />
+        <title>DynMap - RTFMWebsite</title>
+        <meta property="og:title" content="DynMap - RTFMWebsite" />
       </Helmet>
-      <div className="dyn-map-html-node">
-        <span
-          dangerouslySetInnerHTML={{
-            __html:
-              '<!doctype html>\r\n<html lang="en">\r\n\r\n<head>\r\n    <meta charset="utf-8">\r\n    <title>My Dynmap!</title>\r\n    <style>\r\n        * {\r\n            margin: 0;\r\n            padding: 0;\r\n            box-sizing: border-box;\r\n        }\r\n\r\n        body,\r\n        html,\r\n        iframe {\r\n            width: 100%;\r\n            height: 100%;\r\n        }\r\n\r\n        iframe {\r\n            border: none;\r\n        }\r\n\r\n        body {\r\n            display: flex;\r\n        }\r\n    </style>\r\n</head>\r\n\r\n<body>\r\n    <iframe width="100%" height="100%" src="http://mc.freebuilders.org:8123/">\r\n    </iframe>\r\n</body>',
-          }}
-        />
-      </div>
       <div className="dyn-map-container1">
         <Link to="/" className="dyn-map-navlink button">
           <span>
@@ -46,15 +38,11 @@ const DynMap = (props) => {
           </Link>
         </button>
       </div>
-      <div className='.dyn-map-embed'>
-        <body>
-          <iframe width="100%" height="100%" src="https://dynmap.rtfmserver.online/">
-          </iframe>
-        </body>
-      </div>   
-      <span className='copyright'>
-        Copyright © RTFM Group 2022
-      </span>
+      <iframe
+        src="https://dynmap.rtfmserver.online/"
+        allowFullScreen
+        className="dyn-map-iframe"
+      ></iframe>
     </div>
   )
 }
